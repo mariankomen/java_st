72dc7227383505e0db4dd0726bd6ef0c425c259b
Marian Lyzhychka <mr.lezhichka@gmail.com>
2021-05-11 15:04:17 +0300

    Java Scanner

--- src/main.java
+++ src/main.java
@@ -1,12 +1,8 @@
-import java.lang.String; //щоб юзати String
+ import java.util.Scanner;
 
 public class main {
     public static void main (String[] args){
-        byte num = -128;
-        int num_1 = 412341324;
-        float num_2 = 6.456f;
-        double num_3 = 23423423;
-        String str = "hello world";
-        System.out.print(str);
+        Scanner str = new Scanner(System.in);
+        System.out.println("Enter your name: " + str.nextLine());
     }
 }
