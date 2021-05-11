a618ddb2c9e73bba43262cd64e9ac7fa7ff0bedd
Marian Lyzhychka <mr.lezhichka@gmail.com>
2021-05-11 14:51:52 +0300

    first commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main (String[] args){
+        byte num = -128;
+        int num_1 = 412341324;
+        float num_2 = 6.456f;
+        System.out.print(num_1);
+    }
+}
