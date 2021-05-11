f373687fdc4706b95b1be00b211507eadbf5d8cc
Marian Lyzhychka <mr.lezhichka@gmail.com>
2021-05-11 14:58:04 +0300

    Variables

--- src/main.java
+++ src/main.java
@@ -1,8 +1,12 @@
+import java.lang.String; //щоб юзати String
+
 public class main {
     public static void main (String[] args){
         byte num = -128;
         int num_1 = 412341324;
         float num_2 = 6.456f;
-        System.out.print(num_1);
+        double num_3 = 23423423;
+        String str = "hello world";
+        System.out.print(str);
     }
 }
